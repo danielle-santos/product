c641a21a794c8950d414a9b4f4ffd324f7d9a63c
danielle-santos <danielle.danyy92santos@gmail.com>
2019-04-12 21:59:32 -0300

    Integrando JSF com Spring

--- src/main/java/br/com/etechoracio/product/ProductApplication.java
+++ src/main/java/br/com/etechoracio/product/ProductApplication.java
@@ -1,7 +1,11 @@
 package br.com.etechoracio.product;
 
+import javax.faces.webapp.FacesServlet;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ProductApplication {
@@ -10,4 +14,9 @@ public class ProductApplication {
 		SpringApplication.run(ProductApplication.class, args);
 	}
 
+	@Bean
+	public ServletRegistrationBean<FacesServlet> servletRegistrationBean(){
+		return new ServletRegistrationBean<FacesServlet>(new FacesServlet(), "*.xhtml");
+		
+	}
 }
